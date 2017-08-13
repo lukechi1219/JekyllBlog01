138ffd2588725cf1b0cdb82460f8b03451f910b9
Luke Chi <luke.chi@gmail.com>
2017-08-14 00:34:51 +0800

    add precache

---
+++ sw-precache-config.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Luke.Chi on 2017/5/20.
+ */
+
+module.exports = {
+  staticFileGlobs: [
+    '_site/**.html',
+    '_site/**.js',
+    '_site/**.css',
+    '_site/assets/images/*',
+    '_site/assets/icons/*'
+  ],
+  root: '_site',
+  stripPrefix: '_site/',
+  navigateFallback: '/index.html',
+  runtimeCaching: [{
+    urlPattern: /node-hnapi\.herokuapp\.com/,
+    handler: 'networkFirst'
+  }]
+};
